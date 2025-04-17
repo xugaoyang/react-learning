2df595511f1a015685313993c7af747d16ebf078
xugaoyang <yann_xu@outlook.com>
2025-04-17 13:49:14 +0800

    Delete src/pages/article directory

--- src/pages/article/ArticleDetail.jsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom"
-
-function ArticleDetail() {
-  const params = useParams()
-  return <div className="">我是文章{params.id}</div>
-}
-
-export default ArticleDetail
