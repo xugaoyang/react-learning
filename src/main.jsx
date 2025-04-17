84c318c4b749ea8b4acf99a6f204be337187cb08
yann <yann_xu@outlook.com>
2025-04-17 10:59:35 +0800

    feat: antd兼容react19

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import routes from './router'
 import { RouterProvider } from 'react-router-dom'
 import store from './store'
 import { Provider } from 'react-redux'
+import '@ant-design/v5-patch-for-react-19'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
